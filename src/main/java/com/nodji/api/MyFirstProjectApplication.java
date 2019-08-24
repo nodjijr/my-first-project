976fad658f6f6bf71b174090aa84fd7308975742
nodji <nodji.junior@senior.com.br>
2019-08-24 12:38:54 -0300

    alteracao no arquivo principal da aplicacao

--- src/main/java/com/nodji/api/MyFirstProjectApplication.java
+++ src/main/java/com/nodji/api/MyFirstProjectApplication.java
@@ -8,6 +8,7 @@ public class MyFirstProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstProjectApplication.class, args);
+		System.out.println("My First project");
 	}
 
 }
